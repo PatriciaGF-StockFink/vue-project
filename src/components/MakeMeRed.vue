15acd893e843f764693c961d0ed603906a96aff0
PatriciaGF-StockFink <patriciagf.stockfink@gmail.com>
2023-06-22 10:06:37 +0200

    Vue-project

---
+++ src/components/MakeMeRed.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue'
+
+const titleClass = ref('title')
+const input = ref("")
+
+// function updateColor() {
+//   document.documentElement.style.setProperty('--title-color', input.value)
+// }
+</script>
+
+<template>
+  <div class="column">
+    <!-- <h1 :class="titleClass">Make me red</h1> -->
+    <h1 :style="`color: ${input}`">Make me red</h1>
+  	<input v-model="input" type=text/>
+    <p v-if="input">Well done!</p>
+    <p v-else>Type a color</p>
+  </div>
+</template>
+
+<style>
+.title {
+  color: var(--title-color, black)
+}
+</style>
---
+++ src/components/RenderingList.vue
@@ -0,0 +1,54 @@
+<script setup>
+import { ref, computed } from 'vue'
+
+let id = 0
+
+const newTodo = ref('')
+const hideCompleted = ref(false)
+const todos = ref([
+  { id: id++, text: 'Learn HTML', done: true },
+  { id: id++, text: 'Learn JavaScript', done: true },
+  { id: id++, text: 'Learn Vue', done: false }
+])
+
+const filteredTodos = computed(() => {
+  return hideCompleted.value
+  ? todos.value.filter((t) => !t.done)
+  : todos.value
+})
+
+// add new todo object to the array
+function addTodo() {
+  todos.value.push({ id: id++, text: newTodo.value, done: false })
+  newTodo.value = ''
+}
+
+// remove todo by filtering the array
+function removeTodo(todo) {
+  todos.value = todos.value.filter((t) => t !== todo)
+}
+</script>
+
+<template>
+  <form @submit.prevent="addTodo">
+    <input v-model="newTodo">
+    <button>Add Todo</button>
+  </form>
+  <ul>
+    <li v-for="todo in filteredTodos" :key="todo.id">
+      <input type="checkbox" v-model="todo.done">
+      <!-- when todo.done is true, css done class is applied -->
+      <span :class="{ done: todo.done }">{{ todo.text }}</span>
+      <button @click="removeTodo(todo)">X</button>
+    </li>
+  </ul>
+  <button @click="hideCompleted = !hideCompleted">
+    {{ hideCompleted ? 'Show all' : 'Hide completed' }}
+  </button>
+</template>
+
+<style>
+.done {
+  text-decoration: line-through;
+}
+</style>
